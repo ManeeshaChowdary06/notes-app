9ae3828898cda9035788e5e0db20e0aefea4a788
Ravipati Maneesha Chowdary <maneesha@Mac.lan>
2026-02-27 21:23:18 +0530

    notes app

--- backend/app/database.py
+++ backend/app/database.py
@@ -9,7 +9,10 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
     DATABASE_URL,
-    pool_pre_ping=True
+    pool_pre_ping=True,
+    connect_args={"check_same_thread": False}
+    if DATABASE_URL.startswith("sqlite")
+    else {}
 )
 
 SessionLocal = sessionmaker(
